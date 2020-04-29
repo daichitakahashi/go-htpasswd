0ead2442edd8310d6221d050f2c7cc377329058b
高橋大地 <daichitakahashi@mbp.local>
2020-04-29 21:49:52 +0900

    fix AcceptCrypt

--- crypt.go
+++ crypt.go
@@ -13,11 +13,17 @@ type cryptPassword struct {
 
 // AcceptCrypt :
 func AcceptCrypt(src string) (EncodedPasswd, error) {
+	if len(src) != 13 {
+		return nil, nil
+	}
 	return &cryptPassword{src[:2], src}, nil
 }
 
 // RejectCrypt :
 func RejectCrypt(src string) (EncodedPasswd, error) {
+	if len(src) != 13 {
+		return nil, nil
+	}
 	return nil, fmt.Errorf("crypt password rejected: %s", src)
 }
 
